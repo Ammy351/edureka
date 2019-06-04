972bef80b7e53021acaf445922e2c15b466ad659
Amey <ameykambli4@ymail.com>
2019-06-04 21:00:07 +0530

    commit done successfully

---
+++ demo2.java
@@ -0,0 +1,28 @@
+total 483916
+drwxr-xr-x  24 root root      4096 May 19  2018 .
+drwxr-xr-x  24 root root      4096 May 19  2018 ..
+drwxr-xr-x   2 root root      4096 Sep 10  2018 bin
+drwxr-xr-x   3 root root      4096 May 19  2018 boot
+drwxrwxr-x   2 root root      4096 May 19  2018 cdrom
+drwxr-xr-x  18 root root      3900 Jun  2 21:16 dev
+drwxr-xr-x 129 root root     12288 Jun  2 21:16 etc
+drwxr-xr-x   3 root root      4096 May 19  2018 home
+lrwxrwxrwx   1 root root        33 May 19  2018 initrd.img -> boot/initrd.img-4.13.0-21-generic
+drwxr-xr-x  21 root root      4096 May 19  2018 lib
+drwxr-xr-x   2 root root      4096 May 19  2018 lib64
+drwx------   2 root root     16384 May 19  2018 lost+found
+drwxr-xr-x   3 root root      4096 May 19  2018 media
+drwxr-xr-x   2 root root      4096 Jan  6  2018 mnt
+drwxr-xr-x   6 root root      4096 May 22  2018 opt
+dr-xr-xr-x 231 root root         0 Jun  2 21:16 proc
+drwx------   4 root root      4096 May 22  2018 root
+drwxr-xr-x  30 root root       940 Jun  2 21:17 run
+drwxr-xr-x   2 root root     12288 May 22  2018 sbin
+drwxr-xr-x   2 root root      4096 Nov 30  2017 snap
+drwxr-xr-x   2 root root      4096 Jan  6  2018 srv
+-rw-------   1 root root 495416320 May 19  2018 swapfile
+dr-xr-xr-x  13 root root         0 Jun  2 21:16 sys
+drwxrwxrwt  19 root root      4096 Jun  2 21:34 tmp
+drwxr-xr-x  10 root root      4096 Jan  6  2018 usr
+drwxr-xr-x  15 root root      4096 May 22  2018 var
+lrwxrwxrwx   1 root root        30 May 19  2018 vmlinuz -> boot/vmlinuz-4.13.0-21-generic
